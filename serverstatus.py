6cec0a27836ef21fdc5cf86982f8c2154f81bf6f
MichaPehlivan <michapehlivan2004@gmail.com>
2021-09-05 18:41:42 +0200

    added serverstatus.py

---
+++ serverstatus.py
@@ -0,0 +1,17 @@
+from mcstatus import MinecraftServer
+
+server = MinecraftServer("ip adress of server", 25565)
+
+status = server.status()
+online = status.players.online
+
+print(f"there are currently {online}/{status.players.max} players online on {status.description}\n")
+print("version: " + status.version.name)
+print(f"ping: {status.latency} ms\n")
+
+if(online != 0):
+    print("players online: ")
+    for i in status.players.sample:
+        print(i.name)
+
+input()
