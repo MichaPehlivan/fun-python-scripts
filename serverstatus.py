c5765e46bddfd663161e06339ee6b70dcfc1a069
MichaPehlivan <michapehlivan2004@gmail.com>
2021-09-06 15:11:00 +0200

    added online checking to serverstatus

--- serverstatus.py
+++ serverstatus.py
@@ -1,17 +1,21 @@
 from mcstatus import MinecraftServer
 
-server = MinecraftServer("ip adress of server", 25565)
+try:
+    server = MinecraftServer("ip adress of server", 25565)
 
-status = server.status()
-online = status.players.online
+    status = server.status()
+    online = status.players.online
 
-print(f"there are currently {online}/{status.players.max} players online on {status.description}\n")
-print("version: " + status.version.name)
-print(f"ping: {status.latency} ms\n")
+    print(f"there are currently {online}/{status.players.max} players online on {status.description}\n")
+    print("version: " + status.version.name)
+    print(f"ping: {status.latency} ms\n")
 
-if(online != 0):
-    print("players online: ")
-    for i in status.players.sample:
-        print(i.name)
+    if(online != 0):
+        print("players online: ")
+        for i in status.players.sample:
+            print(i.name)
+except:
+    print("server is offline")
+
 
 input()
